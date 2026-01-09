a83e6242a9882aaccd16382edf27b8071972fe4a
ishimura <astr6.6.6@yandex.ru>
2026-01-09 12:58:47 +0300

    Подключил общий логгер проекта

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from src.admin.routes import admin_router
 from src.admin.setup import setup_admin
 from src.db.postgres.config import engine
 from src.config import config
+from logger import setup_logging, get_logger, RequestIdMiddleware
 
+setup_logging(service_name="users-service")
+
+log = get_logger(__name__)
 app = FastAPI()
 
 app.add_middleware(
@@ -14,6 +18,7 @@ app.add_middleware(
     same_site="lax",
     https_only=False if config.APP_ENV == "dev" else True,
 )
+app.add_middleware(RequestIdMiddleware)
 
 app.include_router(admin_router)
 setup_admin(app, engine, session_secret=config.SESSION_SECRET)
